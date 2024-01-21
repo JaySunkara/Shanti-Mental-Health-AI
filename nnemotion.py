f27bd3ea1fbaccf995e65a0294f410622d6dac9a
smathur23 <65054436+smathur23@users.noreply.github.com>
2024-01-21 01:53:38 -0500

    Update nnemotion.py

--- nnemotion.py
+++ nnemotion.py
@@ -9,21 +9,6 @@ nltk.download("wordnet")
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
-
-# import os
-# import collections
-# from sklearn.model_selection import train_test_split
-# import pandas as pd
-# import tensorflow as tf
-# import tensorflow_hub as hub
-# from datetime import datetime
-# import bert
-# from bert import run_classifier
-# from bert import optimization
-# from bert import tokenization
-# from bert import modeling
-
-
 from datasets import load_dataset
 
 dataset = load_dataset("sem_eval_2018_task_1", "subtask5.english")
@@ -41,20 +26,17 @@ def clean_text(text):
         
         return: modified initial string
     """
-    text = text.lower() # lowercase text
-    text = REPLACE_BY_SPACE_RE.sub(' ', text) # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
-    text = BAD_SYMBOLS_RE.sub('', text) # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing. 
+    text = text.lower()
+    text = REPLACE_BY_SPACE_RE.sub(' ', text) 
+    text = BAD_SYMBOLS_RE.sub('', text) 
     text = text.replace('x', '')
-#    text = re.sub(r'\W+', '', text)
-    text = ' '.join(word for word in text.split() if word not in STOPWORDS) # remove stopwors from text
+    text = ' '.join(word for word in text.split() if word not in STOPWORDS)
     return text
 df['Tweet'] = df['Tweet'].apply(clean_text)
 df['Tweet'] = df['Tweet'].str.replace('\d+', '')
 
 MAX_NB_WORDS = 10000
-# Max number of words in each complaint.
 MAX_SEQUENCE_LENGTH = 250
-# This is fixed.
 EMBEDDING_DIM = 100
 tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
 tokenizer.fit_on_texts(df['Tweet'].values)
@@ -116,134 +98,3 @@ padded = pad_sequences(seq, maxlen=MAX_SEQUENCE_LENGTH)
 pred = model.predict(padded)
 labels = ['bruh', 'bruh', 'anger', 'anticipation', 'disgust', 'fear', 'joy', 'love', 'optimism', 'pessimism', 'sadness', 'surprise', 'trust']
 print(pred, labels[np.argmax(pred)])
-
-
-
-
-
-
-
-
-# labels = [label for label in dataset['train'].features.keys() if label not in ['ID', 'Tweet']]
-# id2label = {idx:label for idx, label in enumerate(labels)}
-# label2id = {label:idx for idx, label in enumerate(labels)}
-
-# from transformers import AutoTokenizer
-# import numpy as np
-
-# tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-
-# def preprocess_data(examples):
-#   # take a batch of texts
-#   text = examples["Tweet"]
-#   # encode them
-#   encoding = tokenizer(text, padding="max_length", truncation=True, max_length=128)
-#   # add labels
-#   labels_batch = {k: examples[k] for k in examples.keys() if k in labels}
-#   # create numpy array of shape (batch_size, num_labels)
-#   labels_matrix = np.zeros((len(text), len(labels)))
-#   # fill numpy array
-#   for idx, label in enumerate(labels):
-#     labels_matrix[:, idx] = labels_batch[label]
-
-#   encoding["labels"] = labels_matrix.tolist()
-  
-#   return encoding
-
-# encoded_dataset = dataset.map(preprocess_data, batched=True, remove_columns=dataset['train'].column_names)
-
-# example = encoded_dataset['train'][0]
-
-# tokenizer.decode(example['input_ids'])
-
-# [id2label[idx] for idx, label in enumerate(example['labels']) if label == 1.0]
-
-# encoded_dataset.set_format("torch")
-
-# from transformers import AutoModelForSequenceClassification
-
-# model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", 
-#                                                            problem_type="multi_label_classification", 
-#                                                            num_labels=len(labels),
-#                                                            id2label=id2label,
-#                                                            label2id=label2id)
-
-# batch_size = 8
-# metric_name = "f1"
-
-# from transformers import TrainingArguments, Trainer
-
-# args = TrainingArguments(
-#     f"bert-finetuned-sem_eval-english",
-#     evaluation_strategy = "epoch",
-#     save_strategy = "epoch",
-#     learning_rate=2e-5,
-#     per_device_train_batch_size=batch_size,
-#     per_device_eval_batch_size=batch_size,
-#     num_train_epochs=5,
-#     weight_decay=0.01,
-#     load_best_model_at_end=True,
-#     metric_for_best_model=metric_name,
-# )
-
-# from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
-# from transformers import EvalPrediction
-# import torch
-    
-# def multi_label_metrics(predictions, labels, threshold=0.5):
-#     # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
-#     sigmoid = torch.nn.Sigmoid()
-#     probs = sigmoid(torch.Tensor(predictions))
-#     # next, use threshold to turn them into integer predictions
-#     y_pred = np.zeros(probs.shape)
-#     y_pred[np.where(probs >= threshold)] = 1
-#     # finally, compute metrics
-#     y_true = labels
-#     f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
-#     roc_auc = roc_auc_score(y_true, y_pred, average = 'micro')
-#     accuracy = accuracy_score(y_true, y_pred)
-#     # return as dictionary
-#     metrics = {'f1': f1_micro_average,
-#                'roc_auc': roc_auc,
-#                'accuracy': accuracy}
-#     return metrics
-
-# def compute_metrics(p: EvalPrediction):
-#     preds = p.predictions[0] if isinstance(p.predictions, 
-#             tuple) else p.predictions
-#     result = multi_label_metrics(
-#         predictions=preds, 
-#         labels=p.label_ids)
-#     return result
-
-# outputs = model(input_ids=encoded_dataset['train']['input_ids'][0].unsqueeze(0), labels=encoded_dataset['train'][0]['labels'].unsqueeze(0))
-
-# trainer = Trainer(
-#     model,
-#     args,
-#     train_dataset=encoded_dataset["train"],
-#     eval_dataset=encoded_dataset["validation"],
-#     tokenizer=tokenizer,
-#     compute_metrics=compute_metrics
-# )
-
-# trainer.train()
-# trainer.evaluate()
-
-# text = "I'm happy I can finally train a model for multi-label classification"
-
-# encoding = tokenizer(text, return_tensors="pt")
-# encoding = {k: v.to(trainer.model.device) for k,v in encoding.items()}
-
-# outputs = trainer.model(**encoding)
-
-# logits = outputs.logits
-
-# # apply sigmoid + threshold
-# sigmoid = torch.nn.Sigmoid()
-# probs = sigmoid(logits.squeeze().cpu())
-# predictions = np.zeros(probs.shape)
-# predictions[np.where(probs >= 0.5)] = 1
-# # turn predicted id's into actual label names
-# predicted_labels = [id2label[idx] for idx, label in enumerate(predictions) if label == 1.0]
-# print(predicted_labels)
